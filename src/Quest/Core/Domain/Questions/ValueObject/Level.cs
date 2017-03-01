1092286e14543b503f2871add444d4d5ca7107ee
Pratik Bhattacharya <patrickb404@outlook.com>
2017-03-01 18:20:14 +0530

    Aggregate Root addition started

---
+++ src/Quest/Core/Domain/Questions/ValueObject/Level.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Questions.ValueObject
+{
+    public class Level
+    {
+        public static readonly Level Basic = new Level(100, "Basic");
+        public static readonly Level Amateur = new Level(200, "Amateur");
+        public static readonly Level Professional = new Level(300, "Professional");
+        public static readonly Level Expert = new Level(400, "Expert");
+
+        public int Index { get; private set; }
+        public string DisplayName { get; private set; }
+        public Level(int index, string display)
+        {
+            Index = index;
+            DisplayName = display;
+        }
+        public override string ToString()
+        {
+            return DisplayName;
+        }
+    }
+}
---
+++ src/Quest/Shared/Common/Domain/AggregateRoot.cs
@@ -0,0 +1,7 @@
+﻿namespace Common.Domain
+{
+    public abstract class AggregateRoot
+    {
+        public string Id { get; set; }
+    }
+}
